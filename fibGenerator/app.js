b82b8f718f307f0a912a2d656d11df49d546f5ee
sallyyeng <sallyyeng@gmail.com>
2018-04-08 17:31:32 -0700

    Complete fibGenerator

---
+++ fibGenerator/app.js
@@ -0,0 +1,23 @@
+const number = document.querySelector('.number');
+const btn = document.querySelector('.btn');
+const output = document.querySelector('.display');
+
+const getFibonacci = (num) => {
+  if (typeof Number(num) !== 'number') { return alert('Please enter a number'); }
+  let memo = [0, 1];
+
+  for (; num > 1; num--) {
+    memo.push(memo.shift() + memo[0]);
+  }
+  return memo[num];
+};
+
+const handleClick = (e) => {
+  e.preventDefault();
+
+  const result = getFibonacci(number.value);
+  output.value = result ? result : '';
+  number.value = '';
+};
+
+btn.addEventListener('click', handleClick);
