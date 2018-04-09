1d82884db03b40ba33bdd2892864e31c3f0ce426
sallyyeng <sallyyeng@gmail.com>
2018-04-08 17:30:23 -0700

    Modify to account for other mini-app style.css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('style', function() {
+gulp.task('styleTrafficLights', function() {
   gulp.src('./trafficLights/style.css')
     .pipe(autoprefixer())
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('./trafficLights/build'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watchTrafficLights', function() {
   gulp.watch('style', ['./trafficLights/style.css']);
+});
+
+gulp.task('styleFibGen', function() {
+  gulp.src('./fibGenerator/style.css')
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./fibGenerator/build'));
+});
+
+gulp.task('watchFibGen', function() {
+  gulp.watch('style', ['./fibGenerator/style.css']);
 });
